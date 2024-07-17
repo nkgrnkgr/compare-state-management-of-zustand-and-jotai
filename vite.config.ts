443b81630ba6a3757e1ba1754ed2861ebf5203cd
nkgrnkgr <beefupme@gmail.com>
2024-07-18 08:52:47 +0900

    init

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,23 @@
+import { copyFileSync } from "node:fs";
+import { join } from "node:path";
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: "/state-management-zustand-jotai/",
   plugins: [
     remix({
       ssr: false,
+      basename: "/state-management-zustand-jotai/",
+      buildEnd(args) {
+        if (!args.viteConfig.isProduction) return;
+        const buildPath = args.viteConfig.build.outDir;
+        copyFileSync(
+          join(buildPath, "index.html"),
+          join(buildPath, "404.html"),
+        );
+      },
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
